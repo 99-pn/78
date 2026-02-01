d0ac934b6f60f032d6675e58767c4bdac9ee63bc
99-pn <1403153244@qq.com>
2026-02-01 15:28:41 +0800

    Add files via upload

--- cy-music-ikun-source.js
+++ cy-music-ikun-source.js
@@ -1,5 +1,5 @@
 async function getMusicUrl(songname, artist, songmid, quality) {
-    const targetUrl = `http://music.ikun0014.top/url?source=tx&songId=${songmid}&quality=${quality}`;
+    const targetUrl = `http://api.ikunshare.com/url?source=tx&songId=${songmid}&quality=${quality}`;
     try {
         const response = await fetch(targetUrl, {
             method: 'GET',
@@ -19,8 +19,8 @@ async function getMusicUrl(songname, artist, songmid, quality) {
 module.exports = {
     id: "ikun",
     author: "ikun0014",
-    name: "ikun音源(赞助版)(过期时间2099-12-31 23:59:59)",
+    name: "ikun音源[赞助版][永久]",
     version: "v22",
-    srcUrl: "http://music.ikun0014.top/script/lxmusic?key=KAWANG_b8a6fc01-P1WGLUV5EGUZVTTU",
+    srcUrl: "http://api.ikunshare.com/script/cymusic?key=KAWANG_b8a6fc01-P1WGLUV5EGUZVTTU",
     getMusicUrl
 };
